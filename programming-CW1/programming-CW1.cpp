f10ec54384d08b9d0e53275799434dae745a301f
Karim Hanna <114480303+Khanna1007@users.noreply.github.com>
2024-03-22 13:27:36 +0200

    Update programming-CW1.cpp
    updated code

--- programming-CW1/programming-CW1.cpp
+++ programming-CW1/programming-CW1.cpp
@@ -5,307 +5,258 @@
 
 using namespace std;
 
-// Function to decrypt the password using the Caesar cipher 
-string decryptPassword(string password) {
-    string decryptedPassword = "";
-    for (int i = 0; i < password.length(); i++) {
-        if (isalpha(password[i])) {
-            char c = isupper(password[i]) ? 'A' : 'a';
-            decryptedPassword += char(int(password[i] - c + 26 - 1) % 26 + c);
-        }
-        else {
-            decryptedPassword += password[i];
-        }
-    }
-    return decryptedPassword;
-}
+// Global variable to track the current logged-in users
+string loggedInUser;
 
-// Function to encrypt the password using the Caesar cipher 
+// Encrypts the password using the Caesar cipher
 string encryptPassword(string password) {
     string encryptedPassword = "";
-    for (int i = 0; i < password.length(); i++) {
-        if (isalpha(password[i])) {
-            char c = isupper(password[i]) ? 'A' : 'a';
-            encryptedPassword += char(int(password[i] - c + 1) % 26 + c);
+    for (char i : password) {
+        if (isalpha(i)) {
+            char base = isupper(i) ? 'A' : 'a';
+            encryptedPassword += char(int(i - base + 1) % 26 + base);
         }
         else {
-            encryptedPassword += password[i];
+            encryptedPassword += i;
         }
     }
     return encryptedPassword;
 }
 
-
-
-// Function to delete a password 
-void deletePassword(string username, string website) {
-    ifstream userFile;
-    ofstream tempFile;
-    userFile.open("users.txt");
-    tempFile.open("temp.txt");
-    if (userFile.is_open() && tempFile.is_open()) {
-        string line;
-        bool userFound = false;
-        while (getline(userFile, line)) {
-            string decryptedUsername = decryptPassword(line.substr(0, line.find(":")));
-            if (decryptedUsername == username) {
-                userFound = true;
-                tempFile << line << endl;
-            }
-            else {
-                tempFile << line;
-            }
+// Decrypts the password using the Caesar cipher
+string decryptPassword(string password) {
+    string decryptedPassword = "";
+    for (char i : password) {
+        if (isalpha(i)) {
+            char base = isupper(i) ? 'A' : 'a';
+            decryptedPassword += char((int(i - base - 1) + 26) % 26 + base);
         }
-        userFile.close();
-        tempFile.close();
-
-        ifstream tempUserFile;
-        tempUserFile.open("temp.txt");
-        ofstream newPasswordFile;
-        newPasswordFile.open("newPasswords.txt", ios_base::app);
-        string encryptedUsername = "";
-        string decryptedPassword = "";
-        if (tempUserFile.is_open() && newPasswordFile.is_open()) {
-            while (getline(tempUserFile, line)) {
-                string decryptedUsername = decryptPassword(line.substr(0, line.find(":")));
-                encryptedUsername = line.substr(0, line.find(":"));
-                if (decryptedUsername == username) {
-                    string websiteToDelete = "";
-                    bool passwordFound = false;
-                    while (getline(tempUserFile, line)) {
-                        string decryptedWebsite = decryptPassword(line.substr(0, line.find(":")));
-                        if (decryptedWebsite == website) {
-                            passwordFound = true;
-                        }
-                        else {
-                            if (passwordFound) {
-                                newPasswordFile << line << endl;
-                            }
-                            else {
-                                newPasswordFile << encryptedUsername << ":" << line << endl;
-                            }
-                        }
-                    }
-                    newPasswordFile.close();
-                    remove("passwords.txt");
-                    rename("newPasswords.txt", "passwords.txt");
-                    cout << "Password deleted successfully" << endl;
-                }
-            }
+        else {
+            decryptedPassword += i;
         }
     }
+    return decryptedPassword;
 }
 
-// Function to authenticate a user 
-bool authenticateUser(string username, string password) {
-    ifstream userFile;
-    userFile.open("users.txt");
-    if (userFile.is_open()) {
-        string line;
-        bool userFound = false;
-        while (getline(userFile, line)) {
-            string decryptedUsername = decryptPassword(line.substr(0, line.find(":")));
-            if (decryptedUsername == username) {
-                userFound = true;
-                string decryptedPasswordInFile = decryptPassword(line.substr(line.find(":") + 1));
-                if (decryptedPasswordInFile == password) {
-                    userFile.close();
-                    return true;
-                }
-                else {
-                    cout << "Error: Incorrect password" << endl;
-                    break;
-                }
-            }
-        }
-        userFile.close();
-    }
-    else {
-        cout << "Error: Unable to open the users file" << endl;
-    }
-    return false;
-}
-
-// Function to generate a random password string 
-auto generatePassword(int length) {
-    string password = "";
+// Generates a random password
+string generatePassword(int length) {
     string characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()-_=+{}[]|;:,.<>?";
+    string password = "";
+    srand(time(nullptr)); // Initialize random seed
     for (int i = 0; i < length; i++) {
         password += characters[rand() % characters.length()];
     }
     return password;
 }
 
-// Function to add a new user 
-void addUser(string username, string password) {
-    ofstream userFile;
-    userFile.open("users.txt", ios_base::app);
+// Adds a new user
+void addUser(const string& username, const string& password) {
+    ofstream userFile(username + "_users.txt");
     if (userFile.is_open()) {
-        userFile << encryptPassword(username) << ":" << encryptPassword(password) << endl;
+        userFile << encryptPassword(password) << endl;
         userFile.close();
-        cout << "User added successfully" << endl;
+        cout << "User added successfully." << endl;
     }
     else {
-        cout << "Error: Unable to open the user file" << endl;
+        cout << "Error: Unable to open the user file." << endl;
     }
 }
 
-// Function to retrieve user information 
-string getUserData(string username) {
-    ifstream userFile;
-    userFile.open("users.txt");
+// Authenticates a user
+bool authenticateUser(const string& username, const string& password) {
+    ifstream userFile(username + "_users.txt");
     if (userFile.is_open()) {
-        string line;
-        while (getline(userFile, line)) {
-            string decryptedUsername = decryptPassword(line.substr(0, line.find(":")));
-            if (decryptedUsername == username) {
-                userFile.close();
-                return decryptPassword(line.substr(line.find(":") + 1));
-            }
-        }
+        string storedPassword;
+        getline(userFile, storedPassword);
         userFile.close();
-    }
-    else {
-        cout << "Error: Unable to open the user file" << endl;
-    }
-    return "";
-}
-
-// Function to add a new password 
-void addPassword(const string& website, const string& username, const string& password) {
-    ofstream passwordFile;
-    passwordFile.open("passwords.txt", ios_base::app);
-    if (passwordFile.is_open()) {
-        passwordFile << encryptPassword(website) << ":" << encryptPassword(username) << ":" << encryptPassword(password) << endl;
-        passwordFile.close();
-        cout << "Password added successfully" << endl;
-    }
-    else {
-        cout << "Error: Unable to open the password file" << endl;
-    }
-}
-
-
-// Function to retrieve a password 
-string retrievePassword(const string& username, const string& website) {
-    ifstream passwordFile;
-    passwordFile.open("passwords.txt");
-    if (passwordFile.is_open()) {
-        string line;
-        while (getline(passwordFile, line)) {
-            string decryptedWebsite = decryptPassword(line.substr(0, line.find(":")));
-            string decryptedUsername = decryptPassword(line.substr(line.find(":") + 1, line.rfind(":") - line.find(":") - 1));
-            if (decryptedWebsite == website && decryptedUsername == username) {
-                string decryptedPassword = decryptPassword(line.substr(line.rfind(":") + 1));
-                passwordFile.close();
-                return decryptedPassword;
-            }
+        if (decryptPassword(storedPassword) == password) {
+            loggedInUser = username;
+            return true;
         }
-        passwordFile.close();
-        cout << "Error: Username or website not found" << endl;
     }
-    else {
-        cout << "Error: Unable to open the password file" << endl;
-    }
-    return "";
+    return false;
 }
 
-int main() {
-    int choice;
-    string username, password, website, userInput, encryptedUsername, decryptedPassword;
+// Adds a new password for the currently logged-in user
+void addPassword(const string& website, const string& password) {
+    if (!loggedInUser.empty()) {
+        ofstream passFile(loggedInUser + "_passwords.txt", ios::app);
+        if (passFile.is_open()) {
+            passFile << encryptPassword(website) << ":" << encryptPassword(password) << endl;
+            passFile.close();
+            cout << "Password added successfully." << endl;
+        }
+        else {
+            cout << "Error: Unable to open the password file." << endl;
+        }
+    }
+}
 
-signup_login:
-    cout << "1. Sign up" << endl;
-    cout << "2. Login" << endl;
-    cout << "3. Exit" << endl;
-    cout << "Enter your choice: ";
-    cin >> choice;
-
-    switch (choice) {
-    case 1: // Sign up
-        cout << "Enter a username: ";
-        cin >> username;
-        cout << "Enter a password: ";
-        cin >> password;
-
-        addUser(username, password);
-        cout << "User added successfully" << endl;
-        break;
-    case 2: // Login
-        cout << "Enter your username: ";
-        cin >> username;
-        cout << "Enter your password: ";
-        cin >> password;
-
-        if (authenticateUser(username, password)) {
-            cout << "Login successful" << endl;
-
-            while (true) {
-                cout << "1. Add a new password" << endl;
-                cout << "2. Generate a password" << endl;
-                cout << "3. Retrieve a password" << endl;
-                cout << "4. Delete a password" << endl;
-                cout << "5. Logout" << endl;
-                cout << "Enter your choice: ";
-                cin >> choice;
-
-                switch (choice) {
-                case 1:
-                    cout << "Enter the website: ";
-                    cin >> website;
-                    cout << "Enter Username: ";
-                    cin >> username;
-                    cout << "Enter the password: ";
-                    cin >> password;
-                    addPassword(website, username, password);
-                    break;
-                case 2:
-                    cout << "Enter the website: ";
-                    cin >> website;
-                    cout << "Generated password: " << generatePassword(10) << endl;
-                    break;
-                case 3:
-                    cout << "Enter Username: ";
-                    cin >> username;
-                    cout << "Enter the website: ";
-                    cin >> website;
-                    decryptedPassword = retrievePassword(username, website);
-                    if (decryptedPassword != "") {
-                        cout << "Retrieved password: " << decryptedPassword << endl;
-                    }
-                    else {
-                        cout << "Password not found" << endl;
-                    }
-                    break;
-                case 4:
-                    cout << "Enter Username: ";
-                    cin >> username;
-                    cout << "Enter the website: ";
-                    cin >> website;
-
-                    deletePassword(username, website);
-                    cout << "successfully deleted user and password" << endl;
-                    break;
-                    
-                case 5:
-                    cout << "Logout successful" << endl;
-                    goto signup_login;
-                default:
-                    cout << "Invalid choice. Please try again." << endl;
+// Retrieves a password for the currently logged-in user
+string retrievePassword(const string& website) {
+    if (!loggedInUser.empty()) {
+        ifstream passFile(loggedInUser + "_passwords.txt");
+        if (passFile.is_open()) {
+            string line;
+            while (getline(passFile, line)) {
+                size_t separator = line.find(':');
+                string encryptedWebsite = line.substr(0, separator);
+                string encryptedPassword = line.substr(separator + 1);
+                if (decryptPassword(encryptedWebsite) == website) {
+                    passFile.close();
+                    return decryptPassword(encryptedPassword);
                 }
+            }
+            passFile.close();
+        }
+        else {
+            cout << "Error: Unable to open the password file." << endl;
+        }
+    }
+    return "";
+}
+
+// Deletes a password for the currently logged-in user
+void deletePassword(const string& website) {
+    if (!loggedInUser.empty()) {
+        ifstream passFile(loggedInUser + "_passwords.txt");
+        ofstream tempFile("temp.txt");
+        if (passFile.is_open() && tempFile.is_open()) {
+            string line;
+            bool found = false;
+            while (getline(passFile, line)) {
+                size_t separator = line.find(':');
+                string encryptedWebsite = line.substr(0, separator);
+                if (decryptPassword(encryptedWebsite) != website) {
+                    tempFile << line << endl;
+                }
+                else {
+                    found = true;
+                }
+            }
+            passFile.close();
+            tempFile.close();
+            if (found) {
+                remove((loggedInUser + "_passwords.txt").c_str());
+                rename("temp.txt", (loggedInUser + "_passwords.txt").c_str());
+                cout << "Password for " << website << " deleted successfully." << endl;
+            }
+            else {
+                cout << "No password found for " << website << "." << endl;
+                remove("temp.txt"); // Clean up the temporary file if no matching website was found
             }
         }
         else {
-            cout << "Error: Invalid username or password" << endl;
-            break;
+            cout << "Error: Unable to access the password file." << endl;
         }
-    case 3: // Exit
-        return 0;
-    default:
-        cout << "Invalid choice. Please try again." << endl;
-        break;
+    }
+}
+
+// Function to list all passwords for the currently logged-in user
+void listPasswords() {
+    if (!loggedInUser.empty()) {
+        ifstream passFile(loggedInUser + "_passwords.txt");
+        if (passFile.is_open()) {
+            string line;
+            bool hasPasswords = false;
+            cout << "Stored passwords:" << endl;
+            while (getline(passFile, line)) {
+                size_t separator = line.find(':');
+                string encryptedWebsite = line.substr(0, separator);
+                string encryptedPassword = line.substr(separator + 1);
+                cout << "Website: " << decryptPassword(encryptedWebsite)
+                    << ", Password: " << decryptPassword(encryptedPassword) << endl;
+                hasPasswords = true;
+            }
+            if (!hasPasswords) {
+                cout << "No passwords stored." << endl;
+            }
+            passFile.close();
+        }
+        else {
+            cout << "Error: Unable to open the password file." << endl;
+        }
+    }
+}
+
+int main() {
+    while (true) {
+        int choice;
+        cout << "1. Sign up\n2. Login\n3. Exit\nEnter your choice: ";
+        cin >> choice;
+
+        string username, password, website;
+
+        switch (choice) {
+        case 1: // Sign up
+            cout << "Enter a username: ";
+            cin >> username;
+            cout << "Enter a password: ";
+            cin >> password;
+            addUser(username, password);
+            break;
+        case 2: // Login
+            cout << "Enter your username: ";
+            cin >> username;
+            cout << "Enter your password: ";
+            cin >> password;
+            if (authenticateUser(username, password)) {
+                cout << "Login successful." << endl;
+                bool userSession = true;
+                while (userSession) {
+                    cout << "1. Add a new password\n2. Generate a password\n3. Retrieve a password\n4. Delete a password\n5. List all passwords\n6. Logout\nEnter your choice: ";
+                    cin >> choice;
+                    switch (choice) {
+                    case 1: // Add a new password
+                        cout << "Enter the website: ";
+                        cin >> website;
+                        cout << "Enter the password: ";
+                        cin >> password;
+                        addPassword(website, password);
+                        break;
+                    case 2: // Generate a password
+                        cout << "Generated password: " << generatePassword(10) << endl;
+                        break;
+                    case 3: // Retrieve a password
+                        cout << "Enter the website: ";
+                        cin >> website;
+                        password = retrievePassword(website);
+                        if (!password.empty()) {
+                            cout << "Password: " << password << endl;
+                        }
+                        else {
+                            cout << "No password found for " << website << "." << endl;
+                        }
+                        break;
+                    case 4: // Delete a password
+                        cout << "Enter the website: ";
+                        cin >> website;
+                        deletePassword(website);
+                        break;
+                    case 5: // List all passwords
+                        listPasswords();
+                        break;
+                    case 6: // Logout
+                        loggedInUser.clear();
+                        userSession = false;
+                        cout << "Logged out successfully." << endl;
+                        break;
+                    default:
+                        cout << "Invalid choice. Please try again." << endl;
+                        break;
+                    }
+                }
+            }
+            else {
+                cout << "Invalid username or password." << endl;
+            }
+            break;
+        case 3: // Exit
+            return 0;
+        default:
+            cout << "Invalid choice. Please try again." << endl;
+        }
     }
 
-    // If there was an error or invalid attempt, display options again
-    goto signup_login;
+    return 0;
 }
